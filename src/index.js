422e00b54a87f04b0ed74f711c1beb9ddf2b5340
Subramanya K S <subramanyaks2205@gmail.com>
2022-03-15 19:32:32 +0530

    Initial setup

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
+  <>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </>,
   document.getElementById('root')
 );
 
