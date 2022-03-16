79e6eec7b998ee3bff624cda11bf4b9a79a19a20
Subramanya K S <subramanyaks2205@gmail.com>
2022-03-16 22:48:17 +0530

    updated project

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import 'font-awesome/css/font-awesome.min.css'; 
 
 const Navbar = ()=>{
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <a className="navbar-brand" href="#"><i className="fa fa-code"></i></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
              <span className="navbar-toggler-icon"></span>
         </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
+    <ul className="navbar-nav ms-auto">
       <li className="nav-item active">
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
