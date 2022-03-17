d279cfb75bd57cc4ac6c56f3949839850de9c4f5
Subramanya K S <subramanyaks2205@gmail.com>
2022-03-17 22:51:57 +0530

    updated navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,7 @@ import 'font-awesome/css/font-awesome.min.css';
 const Navbar = ()=>{
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container">
         <a className="navbar-brand" href="#"><i className="fa fa-code"></i></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
              <span className="navbar-toggler-icon"></span>
@@ -15,11 +16,24 @@ const Navbar = ()=>{
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="#">About</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Education</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Experience</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Project</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
       </li>
       
     </ul>
     
+  </div>
   </div>
 </nav>
     )
