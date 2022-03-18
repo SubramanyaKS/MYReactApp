091e7cf0c16b9aae1b2270336640cd1edc31bb89
Subramanya K S <subramanyaks2205@gmail.com>
2022-03-18 22:23:38 +0530

    updated navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import 'font-awesome/css/font-awesome.min.css'; 
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 const Navbar = ()=>{
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
         <a className="navbar-brand" href="#"><i className="fa fa-code"></i></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-             <span className="navbar-toggler-icon"></span>
+          <FontAwesomeIcon icons={faBars} style={{color: "#fff"}}/>
         </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
