95ad8690a510466dc0ed8ddf1b6a8b7212b71a62
Subramanya K S <subramanyaks2205@gmail.com>
2022-03-21 16:38:28 +0530

    contact created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import './App.css';
-
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Header from './components/Header';
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
 import React from "react";
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faGithub, faYoutube, faTwitter, faFacebook,faHackerrank,faLinkedin,faGoogle} from '@fortawesome/free-brands-svg-icons';
 const Contact = () => {
     return(
-        <div>
-            
+        <div className="social-container">
+            <h3>Follow me</h3>
+            <a href="https://www.linkedin.com/in/subramanya-k-s-b74a92204/" className="linkedin social"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a>
+            <a href="https://github.com/SubramanyaKS" className="github social" ><FontAwesomeIcon icon={faGithub} size="2x"/></a>
+			<a href="https://www.hackerrank.com/subramanyaks2205" className ="hackerrank"><FontAwesomeIcon icon={faHackerrank} size="2x"/></a>
+			<a href="https://www.youtube.com/channel/UCg28-MrcK-8CCFos7FWp0Dg/" className="youtube social"><FontAwesomeIcon icon={faYoutube} size="2x"/></a>
+			<a href="https://twitter.com/subbukshebbar" className="twitter social"><FontAwesomeIcon icon={faTwitter} size="2x"/></a>
+			<a href="https://www.facebook.com/profile.php?id=100070944345090"className="facebook social"><FontAwesomeIcon icon={faFacebook} size="2x"/></a>
+            <a href="mailto:subramanyaks2205@gmail.com"className="gmail social"><FontAwesomeIcon icon={faGoogle} size="2x"/></a>
         </div>
     );
-
 }
 export default Contact;
