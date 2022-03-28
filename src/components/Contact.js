680f90aaf2fed0bb11286a56853e524b0300761c
Subramanya K S <subramanyaks2205@gmail.com>
2022-03-28 08:41:53 +0530

    created contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,81 @@
 import React from "react";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import {faGithub, faYoutube, faTwitter, faFacebook,faHackerrank,faLinkedin,faGoogle} from '@fortawesome/free-brands-svg-icons';
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_n17er2k', 'template_8e3unw5', form.current, 'KZLEAm3-rwyXgL5_-')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
     return(
         <div className="social-container">
+            <div class="container">
+      <div class="row">
+        <div class="col align-self-center">
+          <h1 class="text-center">Email - JavaScript Contact Form</h1>
+          {/* <!-- contact form --> */}
+          <form ref={form} onSubmit={sendEmail}>
+            {/* <!-- name --> */}
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input
+                type="name"
+                name="name"
+                class="form-control"
+                id="name"
+                placeholder="enter your name"
+              />
+            </div>
+
+            {/* <!-- email --> */}
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input
+                type="email"
+                name="email"
+                class="form-control"
+                id="email"
+                placeholder="enter your email"
+              />
+            </div>
+
+            {/* <!-- subject --> */}
+            <div class="form-group">
+              <label for="subject">Subject</label>
+              <input
+                type="text"
+                name="subject"
+                class="form-control"
+                id="subject"
+                placeholder="enter email subject"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="email_body">Message</label>
+              <textarea
+                class="form-control"
+                id="email_body"
+                rows="5"
+              ></textarea>
+            </div>
+            <br></br>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <br></br>
             <h3>Let's connect</h3>
 
             <br></br>
