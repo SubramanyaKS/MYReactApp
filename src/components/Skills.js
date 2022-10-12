f698274c44dae9e829cb431150bae145c7c374a6
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-12 10:41:55 +0530

    data formated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,117 +1,118 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
-import { Container, Row} from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 export default function Skills() {
-    return (
-        <>
-        <section className="skills py-5">
-            <Container fluid className="App py-2 overflow-hidden">
-                <Row className="justify-content-center"><h2>Skills</h2></Row>
-                <br></br>
-                <h4> Web Development </h4>
-                <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/html.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/css3.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/javascript.png" />
-                                </Card.Body>
-                            </Card>     
-                </Row>
-                <h4> Programming Language </h4>
-                <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/cprog.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/java.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/python.png" />
-                                </Card.Body>
-                            </Card>      
-                </Row>
-                <h4>DataBase</h4>
-                <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
-                <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/mongodb.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/mysql.png" />
-                                </Card.Body>
-                            </Card>
-                </Row>
-                <h4>Web FrameWork</h4>
-                <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
-                    <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/react.png" />
-                                </Card.Body>
-                    </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/flask.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/django.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/bootstrap.png" />
-                                </Card.Body>
-                            </Card>
-                </Row>
-                <h4>Tools</h4>
-                <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
-                <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/git.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/android.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/jupyter.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/vscode.png" />
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: "6rem" }}>
-                                <Card.Body>
-                                <Card.Img  src="./images/eclipse.png" />
-                                </Card.Body>
-                            </Card>
-                </Row>
-                 </Container>
-        </section>
-        </>
-    );
-
+  return (
+    <>
+      <section className="skills py-5">
+        <Container fluid className="App py-2 overflow-hidden">
+          <Row className="justify-content-center">
+            <h2>Skills</h2>
+          </Row>
+          <br></br>
+          <h4> Web Development </h4>
+          <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/html.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/css3.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/javascript.png" />
+              </Card.Body>
+            </Card>
+          </Row>
+          <h4> Programming Language </h4>
+          <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/cprog.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/java.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/python.png" />
+              </Card.Body>
+            </Card>
+          </Row>
+          <h4>DataBase</h4>
+          <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/mongodb.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/mysql.png" />
+              </Card.Body>
+            </Card>
+          </Row>
+          <h4>Web FrameWork</h4>
+          <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/react.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/flask.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/django.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/bootstrap.png" />
+              </Card.Body>
+            </Card>
+          </Row>
+          <h4>Tools</h4>
+          <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/git.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/android.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/jupyter.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/vscode.png" />
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "6rem" }}>
+              <Card.Body>
+                <Card.Img src="./images/eclipse.png" />
+              </Card.Body>
+            </Card>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 }
