ad2671aa50dbac292d59b6616a03d3c58e445427
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-12 10:31:56 +0530

    added footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ const Header = () => {
     return (
         <section  className="main py-5">
 	        <div className="container-fluid py-5">
-                <br></br><br></br>
+                <br></br>
 		        <div className="row py-5">
 			        <div className = "col-lg-12 col-mg-12 py-5 mr-auto">
                        <br></br>
                         <h1>Hi I am </h1>
-                        <h1>SUBRAMANYA K S</h1>
+                        <h1 style ={{fontSize:"50px"}}>SUBRAMANYA K S</h1>
                         <br></br>
                         <br></br>
                 <Typed
@@ -20,7 +20,7 @@ const Header = () => {
                 backSpeed={60}
                 loop
                 />
-                <br></br>
+            
             </div>
 
         </div>
