08df37e2e98073660631039fe247afccc3596f7c
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-12 10:41:26 +0530

    data formated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,20 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHeart} from '@fortawesome/free-solid-svg-icons';
-const Footer=()=>{
-    return(
-        <div className ="footer">
-            <p class="text-center" ><b>&copy;2022. Designed with <FontAwesomeIcon icon={faHeart} style={{color:"red",fontSize: '20px'}} beat/> by
-				Subramanya K S.</b></p>
-        </div>
-
-    );
-
-}
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p class="text-center">
+        <b>
+          &copy;2022. Designed with{" "}
+          <FontAwesomeIcon
+            icon={faHeart}
+            style={{ color: "red", fontSize: "20px" }}
+            beat
+          />{" "}
+          by Subramanya K S.
+        </b>
+      </p>
+    </div>
+  );
+};
 export default Footer;
