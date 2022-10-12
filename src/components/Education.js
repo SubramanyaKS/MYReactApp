7a4b3d5cfa1d83b1480465b24c1b90f626bf4ea4
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-12 23:02:13 +0530

    updated react style tags

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,13 +6,13 @@ const Education = () => {
       <section className="education py-5" id="education">
         <div className="container-fluid">
           <div className="row">
-            <h1 className="mb-5" style={{ color: "aqua" }}>
+            <h1 className="mb-5" style={{ color: "#00ddff" }}>
               {" "}
-              &nbsp; Education Details.
+              &nbsp; &lt; Education Details. /&gt;
             </h1>
             <div className="col">
-              <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div className="shadow-lg flex-grow-1">
+              <div className="d-flex box flex-column flex-md-row justify-content-between mb-5">
+                <div className="shadow-lg  flex-grow-1">
                   <h3 className="mb-0">
                     &nbsp;J N N College of Engineering, Shivamogga
                   </h3>
@@ -30,11 +30,11 @@ const Education = () => {
                   </p>
                 </div>
               </div>
-              <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+              <div className="d-flex box flex-column flex-md-row justify-content-between mb-5">
                 <div className="shadow-lg flex-grow-1">
                   <h3 className="mb-0">&nbsp;Vikasa P U College, Shivamogga</h3>
                   <p className="mb-0" style={{ fontSize: "1.25rem" }}>
-                    {" "}
+                    {" "}<br/>
                     &nbsp; PUC /12th
                   </p>
                   <p style={{ fontSize: "1.0rem" }}>
@@ -42,13 +42,13 @@ const Education = () => {
                   </p>
                 </div>
               </div>
-              <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+              <div className="d-flex box flex-column flex-md-row justify-content-between mb-5">
                 <div className="shadow-lg flex-grow-1">
                   <h3 className="mb-0">
                     &nbsp;Sri Ramakrishna Vidyanikethana, Shivamogga
                   </h3>
                   <p className="mb-0" style={{ fontSize: "1.25rem" }}>
-                    {" "}
+                    {" "}<br/>
                     &nbsp; SSLC/10th
                   </p>
                 </div>
