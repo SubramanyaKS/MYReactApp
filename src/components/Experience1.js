53b51306c8ab84d14c8f71a7e2457cdaa44fc3a7
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-12 10:41:20 +0530

    data formated

--- src/components/Experience1.js
+++ src/components/Experience1.js
@@ -1,52 +1,79 @@
 import Card from "react-bootstrap/Card";
 import React from "react";
-import 'font-awesome/css/font-awesome.min.css'; 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import "font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Experience1() {
   return (
     <>
-    <section className="experience py-5" id="experience">
-  <div className="container-fluid">
-    <h1 style={{color:"aqua"}}> &nbsp; Experience.</h1>
-    <br /><br/>
-    <div className="row row-cols-1 row-cols-md-2">
-      <div className="col sm-6  d-flex align-items-stretch">
-      <Card style={{ width: "25rem" }}>
-      <Card.Img variant="top" src="./images/ekatvaI.jpg" />
-        <Card.Body>
-          <Card.Title style={{ color: "aqua" }}>Web Development Intern.</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-          Ekathva Innovations Pvt. Ltd.
-          </Card.Subtitle>
-          <Card.Text>
-          Done a Project titled "Django Based REST API development for Ayucare Management".One month online industry level internship helped me to gain knowledge about resent technology, ideologies and aesthetics of the company.Learnt about Django and REST API
-          </Card.Text>
-          <div className="social-buttons mt-5"><a href="https://github.com/SubramanyaKS/AyuCareManagement"> <button className="neo-button"><FontAwesomeIcon icon={faGithub} style={{fontSize: '24px'}}/> </button></a></div>
-        </Card.Body>
-      </Card>
-    </div>
-    <div className="col sm-6  d-flex align-items-stretch">
-      <Card style={{ width: "25rem" }}>
-      <Card.Img variant="top" src="./images/Thesparks.jpg" />
-        <Card.Body>
-          <Card.Title style={{ color: "aqua" }}>Web Development and Design Intern.</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-          The Sparks Foundation.
-          </Card.Subtitle>
-          <Card.Text>
-          My First Internship helped me to gain some knowledge on web development. Worked on the project title "Basic Banking System" which is demonstrate the basic banking operation.
-          </Card.Text>
-          <div className="social-buttons mt-5"><a href="https://github.com/SubramanyaKS/Task1-BasicBankingSystem"> <button className="neo-button"><FontAwesomeIcon icon={faGithub} style={{fontSize: '24px'}}/> </button></a></div>
-        </Card.Body>
-      </Card>
-    </div>
-    
-
-      </div>
-      
-      </div>
-      
+      <section className="experience py-5" id="experience">
+        <div className="container-fluid">
+          <h1 style={{ color: "aqua" }}> &nbsp; Experience.</h1>
+          <br />
+          <br />
+          <div className="row row-cols-1 row-cols-md-2">
+            <div className="col sm-6  d-flex align-items-stretch">
+              <Card style={{ width: "25rem" }}>
+                <Card.Img variant="top" src="./images/ekatvaI.jpg" />
+                <Card.Body>
+                  <Card.Title style={{ color: "aqua" }}>
+                    Web Development Intern.
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Ekathva Innovations Pvt. Ltd.
+                  </Card.Subtitle>
+                  <Card.Text>
+                    Done a Project titled "Django Based REST API development for
+                    Ayucare Management".One month online industry level
+                    internship helped me to gain knowledge about resent
+                    technology, ideologies and aesthetics of the company.Learnt
+                    about Django and REST API
+                  </Card.Text>
+                  <div className="social-buttons mt-5">
+                    <a href="https://github.com/SubramanyaKS/AyuCareManagement">
+                      {" "}
+                      <button className="neo-button">
+                        <FontAwesomeIcon
+                          icon={faGithub}
+                          style={{ fontSize: "24px" }}
+                        />{" "}
+                      </button>
+                    </a>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col sm-6  d-flex align-items-stretch">
+              <Card style={{ width: "25rem" }}>
+                <Card.Img variant="top" src="./images/Thesparks.jpg" />
+                <Card.Body>
+                  <Card.Title style={{ color: "aqua" }}>
+                    Web Development and Design Intern.
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    The Sparks Foundation.
+                  </Card.Subtitle>
+                  <Card.Text>
+                    My First Internship helped me to gain some knowledge on web
+                    development. Worked on the project title "Basic Banking
+                    System" which is demonstrate the basic banking operation.
+                  </Card.Text>
+                  <div className="social-buttons mt-5">
+                    <a href="https://github.com/SubramanyaKS/Task1-BasicBankingSystem">
+                      {" "}
+                      <button className="neo-button">
+                        <FontAwesomeIcon
+                          icon={faGithub}
+                          style={{ fontSize: "24px" }}
+                        />{" "}
+                      </button>
+                    </a>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
