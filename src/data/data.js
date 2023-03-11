cf35b9e408fcba2dc873066a7673e2e6f7cade0a
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-11 07:46:44 +0530

    restructured project

---
+++ src/data/data.js
@@ -0,0 +1,89 @@
+export const projectsdata =[
+    {
+      id:1,
+      title: "Weather Application",
+      subtitle: "API Based Web Application",
+      description: "Developed a web application which uses openweathermap API to fetch the weather details of various city across the globe by giving city name as input.Used Django as web framework.",
+      image: "./images/weather.jpg",
+      githubLink: "https://github.com/SubramanyaKS/Weather-app/",
+      websiteLink:"https://subramanyaks.github.io/Weather-app",
+      downloadLink:null
+    },
+    {
+        id:2,
+        title: "Media Player Application",
+        subtitle: "Android application.",
+        description:
+          "Application which play the audio and video and able to move forward and backword of media using button.",
+        image: "./images/media.jpg",
+        githubLink: "https://github.com/SubramanyaKS/MediaPlayer-Android-Application",
+        websiteLink:null,
+        downloadLink:null
+      },
+      {
+        id:3,
+        title: "Bus Reservation System",
+        subtitle: "A Dbms mini-project",
+        description:
+          "This Project is the DBMS mini-project and desktop application where we can reserve the bus seat,check the seat availability and view the seat arrangement do the payment after seat reservation.",
+        image: "./images/bus.jpg",
+        githubLink: "https://github.com/SubramanyaKS/busreservation",
+        websiteLink:null,
+        downloadLink:"https://github.com/SubramanyaKS/busreservation/blob/main/BusReservation.jar"
+      },
+        {
+          id:4,
+          title: "Random Quote Generator.",
+          subtitle: "html javascript based web app.",
+          description:
+            "A simple web application which display the quote with both text and speech using random quote api.",
+          image: "./images/quote.jpg",
+          githubLink: "https://github.com/SubramanyaKS/RandomQuoteGenerator",
+          websiteLink:"https://subramanyaks.github.io/RandomQuoteGenerator",
+          downloadLink:null
+        },
+        {
+          id:5,
+          title: "Electric Energy monitoring System.",
+          subtitle: "IoT and Web based project.",
+          description:
+            "This project based on IoT and web based electric energy monitoring system. Used Nodemcu and flask",
+          image: "./images/energy.jpg",
+          githubLink: "https://github.com/SubramanyaKS/Final-year-project",
+          websiteLink:null,
+          downloadLink:null
+        },
+        {
+          id:6,
+          title: "VTU SGPA CGPA Calculator.",
+          subtitle: "Java jar application.",
+          description:
+            "This project  which calculate the CGPA and SGPA of VTU students.Created both java jar application and android app",
+          image: "./images/cal.jpg",
+          githubLink: "https://github.com/SubramanyaKS/VTU-CGPA_SGPA-Calculator/",
+          websiteLink:null,
+          downloadLink:"https://github.com/SubramanyaKS/Vtu-sgpa-calculator/blob/main/Vtu_SGPACGPA_Calculator_1.0.apk"
+        },
+        {
+          id:7,
+          title: "Heart Disease Prediction System.",
+          subtitle: "Machine-learning project using regression model.",
+          description:
+            "This project predict the heart disease by various machine learning model using ‘heart.csv’ dataset.",
+          image: "./images/heart.jpg",
+          githubLink: "https://github.com/SubramanyaKS/Heart_Attack_Prediction",
+          websiteLink:null,
+          downloadLink:null,
+        },
+        {
+          id:8,
+          title: "Diabeties Prediction System.",
+          subtitle: "Machine-learning project using regression model.",
+          description:
+            "In this project we determine the diabetes by building various machine learning model using ‘diabetes.csv’ dataset.",
+          image: "./images/diabeties.jpg",
+          githubLink: "https://github.com/SubramanyaKS/Diabetes_prediction",
+          websiteLink:null,
+          downloadLink:null
+        }
+]
