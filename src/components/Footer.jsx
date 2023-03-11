13783073295a2e9ca4065a1f59f16bb90a90ea57
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-11 12:26:47 +0530

    moved files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p  style={{fontFamily:"cursive"}} className="text-center">
+        <b>
+          &copy;2022. Designed with{" "}
+          <FontAwesomeIcon
+            icon={faHeart}
+            style={{ color: "red", fontSize: "20px" }}
+            className="fa-beat"
+          />{" "}
+          by &lt;Subramanya K S./&gt;
+        </b>
+      </p>
+    </div>
+  );
+};
+export default Footer;
