e6ef42a12aaecfe7599bc22c13e8c057c375bacb
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-11 16:24:13 +0530

    restructured Education

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Navbar from "./components/NavBar1";
 import About from "./pages/About";
 import Header from "./pages/Header";
-import Education from "./components/Education";
+import Education from "./pages/Education";
 import Skills from "./pages/Skills";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from "./pages/Contact";
