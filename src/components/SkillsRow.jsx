235c40daaa55cae622ad9d4fe321f0eab3b96f3e
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-11 11:42:57 +0530

    restructured skills

---
+++ src/components/SkillsRow.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+import SkillsCard from "./SkillsCard";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const SkillsRow = ({ skill }) => {
+  return (
+    <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+      {skill.map((item) => (
+        <SkillsCard key={item.id} data={item} />
+      ))}
+    </Row>
+  );
+};
+
+export default SkillsRow;
