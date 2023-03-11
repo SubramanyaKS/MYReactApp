4c6e97b97f066b525c9b5d2a8780f39d2b796f57
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-11 12:26:51 +0530

    moved files

--- src/data/data.js
+++ src/data/data.js
@@ -183,6 +183,13 @@ export const other=[
   }
 ]
 
+export const aboutdetails ={
+  name:"Subramanya KS",
+  image:"./images/subbu.png",
+  tagline:"Learner|Programmer|Developer",
+  description:"A curious minded individual who is eager to explore and solve different problems, and, doing so I try out new approaches with existing technologies, experiment with emerging tools, constantly updating my knowledge, thereby getting better at it each time. I'm passionate about Web Development, Android App Development. Accordingly, I'm equipped with HTML, CSS, JavaScript, SQL skills and proficient in OOP concepts and languages like Java and C. Utilizing all of these skills, I have completed several projects regarding the same and thus am aware of the real world practical constraints that is usually encountered while solving problems. I also actively interact with the open source community, contributing to it and also trying out new stuff."
+}
+
 export const experiencedata=[
   {
     id:1,
