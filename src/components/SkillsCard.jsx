d2a6b3cb0875237234632a0e845f0d9e9d1b0417
Subramanya K S <subramanyaks2205@gmail.com>
2023-04-10 22:14:36 +0530

    updated skill img

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
-
+import '../assets/skill.css'
 const SkillsCard = ({ data }) => {
   return (
     <Card style={{ width: "6rem" }}>
