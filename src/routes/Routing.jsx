e5a436d42ecf452587b757d14edff5e4b29e3c0b
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-07 16:48:45 +0530

    seperated routing

---
+++ src/routes/Routing.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from "../components/NavBar";
+import About from "../pages/About";
+import Header from "../pages/Header";
+import Education from "../pages/Education";
+import Skills from "../pages/Skills";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from "../pages/Contact";
+import Footer from "../components/Footer";
+import Projects from "../pages/Projects";
+import Experience from "../pages/Experience";
+
+const Routing = () => {
+  return (
+    <Router>
+       <Navbar />
+       <Routes>
+         <Route exact path="/" element={<Header />} />
+         <Route exact path="/about" element={<About />} />
+         <Route exact path="/education" element={<Education />} />
+         <Route exact path="/experience" element={<Experience />} />
+         <Route exact path="/project" element={<Projects />} />
+         <Route exact path="/skills" element={<Skills />} />
+         <Route exact path="/contact" element={<Contact />} />
+       </Routes>
+       <Footer />
+     </Router>
+  )
+}
+
+export default Routing
