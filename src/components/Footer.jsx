c2036f2671d96c395a7575df1fea22dba739b280
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-08 23:04:43 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
   return (
     <div className="footer">
       <p  style={{fontFamily:"cursive"}} className="text-center">
         <b>
-          &copy;2023. Designed with{" "}
+          &copy;2024. Designed with{" "}
           <FontAwesomeIcon
             icon={faHeart}
             style={{ color: "red", fontSize: "20px" }}
