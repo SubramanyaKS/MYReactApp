6f7bd81ea1899d5e12bf5e8a9b04eace78293aa2
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-13 22:48:22 +0530

    updated contact

--- src/components/ContactIcons.jsx
+++ src/components/ContactIcons.jsx
@@ -1,30 +1,18 @@
 import React from 'react'
-import {
-    faGithub,
-    faYoutube,
-    faTwitter,
-    faFacebook,
-    faLinkedin,
-    faGoogle,
-    faInstagram,
-  } from "@fortawesome/free-brands-svg-icons";
-  import IconContainer from "../components/IconContainer";
+import IconContainer from "../components/IconContainer";
+import { socialIcon } from '../data/data';
+import Center from './common/Center';
 
 const ContactIcons = () => {
   return (
-    <div className='d-flex mb-3 mt-3 justify-content-center align-items-center'>
-        <IconContainer  href="https://www.linkedin.com/in/subramanyakshebbar/"
-          className="linkedin social" icon={faLinkedin} iconClass={"fa-bounce"}/>
-         <IconContainer href="https://github.com/SubramanyaKS" className="github social" icon={faGithub} iconClass={'fa-flip'} />
-        <IconContainer href="https://www.youtube.com/@subramanyakshebbar/"
-          className="youtube social" icon={faYoutube} iconClass={'fa-beat'}/>
-        <IconContainer href="https://twitter.com/subbukshebbar" className="twitter social" icon={faTwitter} iconClass={"fa-shake"}/>
-        <IconContainer href="https://www.facebook.com/profile.php?id=100070944345090"
-          className="facebook social" icon={faFacebook} iconClass={'fa-fade'}/>
-        <IconContainer href="https://www.instagram.com/subramanyakshebbar/"
-          className="instagram social" icon={faInstagram} iconClass={'fa-spin'} />
-        <IconContainer icon={faGoogle} href="mailto:subramanyaks2205@gmail.com" className="gmail social" iconClass={'fa-beat-fade'}/>
-    </div>
+    <Center>
+      {
+        socialIcon.map((data)=>(
+          <IconContainer  href={data.href}
+          className={data.className} icon={data.icon} iconClass={data.iconClass}/>
+        ))
+      }
+    </Center>
   )
 }
 
