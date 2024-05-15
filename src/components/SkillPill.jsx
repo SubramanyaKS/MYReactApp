1bccb7fe30e851521deaae13d92b1973516b8939
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-15 06:21:58 +0530

    Added a skill pill for product card

---
+++ src/components/SkillPill.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SkillPill = ({text}) => {
+  return (
+    <div className='bg-primary p-2 rounded-pill'>
+      {text}
+    </div>
+  )
+}
+
+export default SkillPill
