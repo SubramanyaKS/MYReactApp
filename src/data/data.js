7d7bbe3797ac513255e238db672cadac6897876e
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-16 23:19:25 +0530

    updated data

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,13 @@
+import {
+  faGithub,
+  faYoutube,
+  faTwitter,
+  faFacebook,
+  faLinkedin,
+  faGoogle,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 export const projectsdata =[
     {
       id:1,
@@ -5,83 +15,103 @@ export const projectsdata =[
       subtitle: "API Based Web Application",
       description: "Developed a web application which uses openweathermap API to fetch the weather details of various city across the globe by giving city name as input.Used Django as web framework.",
       image: "./images/weather.jpg",
+      techStack:["React JS","Bootstrap"],
       githubLink: "https://github.com/SubramanyaKS/Weather-app/",
       websiteLink:"https://subramanyaks.github.io/Weather-app",
       downloadLink:null
     },
     {
         id:2,
-        title: "Media Player Application",
-        subtitle: "Android application.",
+        title: "React News App",
+        subtitle: "API Based Web Application.",
         description:
-          "Application which play the audio and video and able to move forward and backword of media using button.",
-        image: "./images/media.jpg",
-        githubLink: "https://github.com/SubramanyaKS/MediaPlayer-Android-Application",
+          "React JS-based online news web app that utilizes the NewsAPI.org API. It allows users to access and read news articles from a wide range of sources.",
+        image: "./images/news.jpg",
+        techStack:["React JS","Bootstrap"],
+        githubLink: "https://github.com/SubramanyaKS/React-News-App/",
         websiteLink:null,
         downloadLink:null
       },
       {
         id:3,
+        title: "Media Player Application",
+        subtitle: "Android application.",
+        description:
+          "Application which play the audio and video and able to move forward and backword of media using button.",
+        image: "./images/media.jpg",
+        techStack:["Android","Java"],
+        githubLink: "https://github.com/SubramanyaKS/MediaPlayer-Android-Application",
+        websiteLink:null,
+        downloadLink:null
+      },
+      {
+        id:4,
         title: "Bus Reservation System",
         subtitle: "A Dbms mini-project",
         description:
           "This Project is the DBMS mini-project and desktop application where we can reserve the bus seat,check the seat availability and view the seat arrangement do the payment after seat reservation.",
         image: "./images/bus.jpg",
+        techStack:["Java","Oracle db"],
         githubLink: "https://github.com/SubramanyaKS/busreservation",
         websiteLink:null,
         downloadLink:"https://github.com/SubramanyaKS/busreservation/blob/main/BusReservation.jar"
       },
         {
-          id:4,
+          id:5,
           title: "Random Quote Generator.",
           subtitle: "html javascript based web app.",
           description:
             "A simple web application which display the quote with both text and speech using random quote api.",
           image: "./images/quote.jpg",
+          techStack:["Javascript","CSS","HTML","Bootstrap"],
           githubLink: "https://github.com/SubramanyaKS/RandomQuoteGenerator",
           websiteLink:"https://subramanyaks.github.io/RandomQuoteGenerator",
           downloadLink:null
         },
         {
-          id:5,
+          id:6,
           title: "Electric Energy monitoring System.",
           subtitle: "IoT and Web based project.",
           description:
             "This project based on IoT and web based electric energy monitoring system. Used Nodemcu and flask",
           image: "./images/energy.jpg",
+          techStack:["Flask","Arudino"],
           githubLink: "https://github.com/SubramanyaKS/Final-year-project",
           websiteLink:null,
           downloadLink:null
         },
         {
-          id:6,
+          id:7,
           title: "VTU SGPA CGPA Calculator.",
           subtitle: "Java jar application.",
           description:
             "This project  which calculate the CGPA and SGPA of VTU students.Created both java jar application and android app",
           image: "./images/cal.jpg",
+          techStack:["Java","Android"],
           githubLink: "https://github.com/SubramanyaKS/VTU-CGPA_SGPA-Calculator/",
           websiteLink:null,
           downloadLink:"https://github.com/SubramanyaKS/Vtu-sgpa-calculator/blob/main/Vtu_SGPACGPA_Calculator_1.0.apk"
         },
         {
-          id:7,
+          id:8,
           title: "Heart Disease Prediction System.",
           subtitle: "Machine-learning project using regression model.",
           description:
             "This project predict the heart disease by various machine learning model using ‘heart.csv’ dataset.",
           image: "./images/heart.jpg",
+          techStack:["Python","Jupyter Notebook"],
           githubLink: "https://github.com/SubramanyaKS/Heart_Attack_Prediction",
           websiteLink:null,
           downloadLink:null,
         },
         {
-          id:8,
+          id:9,
           title: "Diabeties Prediction System.",
           subtitle: "Machine-learning project using regression model.",
           description:
             "In this project we determine the diabetes by building various machine learning model using ‘diabetes.csv’ dataset.",
           image: "./images/diabeties.jpg",
+          techStack:["Python","Jupyter Notebook"],
           githubLink: "https://github.com/SubramanyaKS/Diabetes_prediction",
           websiteLink:null,
           downloadLink:null
@@ -186,6 +216,7 @@ export const other=[
 export const aboutdetails ={
   name:"Subramanya KS",
   image:"./images/subbu.png",
+  resumeLink:"https://drive.google.com/file/d/15V_I5zW-iQ8eYWvtQEmi6sZ-6DgWGvya/view",
   tagline:"Learner|Programmer|Developer",
   description:" A frontend developer based in Karnataka,India. I am currently working in Infosys. I passionate about upgrading my skills and also likes to get my hand dirty on new technology. I start my day with motive on being better then previous day. Learn new skills that helped me to do better work.I'm constantly open to new ideas and always look to reach out to others, share and discuss ideas with them, thereby learn and also help others grow too"
 }
@@ -208,6 +239,68 @@ export const experiencedata=[
     githubLink: "https://github.com/SubramanyaKS/Task1-BasicBankingSystem/",
   }
 ]
+export const socialIcon=[
+  {
+    id:1,
+    href:"https://www.linkedin.com/in/subramanyakshebbar/",
+    className:"linkedin social",
+    icon:faLinkedin,
+    iconClass:"fa-bounce"
+  },
+  {
+    id:2,
+    href:"https://github.com/SubramanyaKS",
+    className:"github social",
+    icon:faGithub,
+    iconClass:"fa-flip"
+  },
+  {
+    id:3,
+    href:"https://www.youtube.com/@subramanyakshebbar/",
+    className:"youtube social",
+    icon:faYoutube,
+    iconClass:"fa-beat"
+  },
+  {
+    id:4,
+    href:"https://twitter.com/subbukshebbar",
+    className:"twitter social",
+    icon:faTwitter,
+    iconClass:"fa-shake"
+  },
+  {
+    id:5,
+    href:"https://www.facebook.com/profile.php?id=100070944345090",
+    className:"facebook social",
+    icon:faFacebook,
+    iconClass:"fa-fade"
+  },
+  {
+    id:6,
+    href:"https://www.instagram.com/subramanyakshebbar/",
+    className:"instagram social",
+    icon:faInstagram,
+    iconClass:"fa-spin"
+  },
+  {
+    id:7,
+    href:"mailto:subramanyaks2205@gmail.com",
+    className:"gmail social",
+    icon:faGoogle,
+    iconClass:"fa-beat-fade"
+  }
+
+]
+
+export const headerdata={
+  image:"./images/avatar.png",
+  name:"SUBRAMANYA K S",
+  typedarr:["Learner",
+  "Developer",
+  "Simple Coder",
+  "MERN Stack Developer",
+  "Open Source Contributor"]
+}
 
 export const educationdata =[
   {
