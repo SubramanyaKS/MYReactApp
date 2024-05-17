70e3d9aa591d3ec59f7d863951b12e6f9745e7c3
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-17 23:15:27 +0530

    updated project section with formor motion

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import {projectsdata} from '../data/data.js';
-import ProjectCard from '../components/ProjectCard.jsx';
-import Background from '../components/Background.js';
-
+import React from "react";
+import { projectsdata } from "../data/data.js";
+import ProjectCard from "../components/ProjectCard.jsx";
+// import Background from "../components/Background.js";
+import { Col, Container, Row } from "react-bootstrap";
+import Heading from "../components/common/Heading.jsx";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <section className="project py-5" id="project">
-      <div className="container-fluid ">
-          <h1 className="text-center mb-5" style={{ color: "#00ddff" }}>
-            {" "}
-            &nbsp;&lt; My Project. /&gt;
-          </h1>
-          <div className="row row-cols-1 row-cols-md-2 row-eq-height">
-            
-      {projectsdata.map((project) => (
-        <div className="col mb-4  d-flex align-items-stretch">
-           <ProjectCard data={project} />
-           </div> 
+      <Heading title="My Projects" />
+      <Container fluid>
+        <Row className="row-cols-1 row-cols-md-3 row-eq-height">
+          {projectsdata.map((project,i) => (
+            <Col className="mb-4  d-flex align-items-center justify-content-center">
+              <motion.div
+                key={project.id}
+                initial={{ opacity: 0, translateX: -50 , translateY:-50}}
+                animate={{ opacity: 1, translateX: 0, translateY:0 }}
+                transition={{duration:0.3, delay:i+0.1}}
+              >
+                <ProjectCard data={project} />
+              </motion.div>
+            </Col>
           ))}
-     
-      </div>
-      </div>
-      <Background id="tsparticles" />
+        </Row>
+        <button onClick={()=>window.open(
+      "https://github.com/SubramanyaKS?tab=repositories",
+      "_blank"
+    )} class="resume rounded-pill">View More</button>
+      </Container>
+      {/* <Background id="tsparticles" /> */}
     </section>
-  )
-}
+  );
+};
 
 export default Projects;
