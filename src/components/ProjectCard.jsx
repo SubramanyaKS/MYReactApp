57ba3a8d8c9d78bcd015873963408f6fc7a33d82
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-17 23:15:35 +0530

    updated project section with formor motion

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React,{useState} from 'react';
 // import { projectsdata } from '../data/data';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faDownload, faFolderOpen, faStar as Star } from "@fortawesome/free-solid-svg-icons";
 import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
 import Card from "react-bootstrap/Card";
+import { faFolder, faStar } from '@fortawesome/free-regular-svg-icons';
+import Reveal from './common/Reveal';
+import SkillPill from './SkillPill';
 
 const ProjectCard = ({data}) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-         <Card style={{ width: "25rem", fontFamily: "cursive" }}>
+         <Card onMouseEnter={handleMouseEnter}
+         onMouseLeave={handleMouseLeave} style={{ width: "25rem", fontFamily: "cursive" }}>
                 <Card.Img variant="top" src={data.image} />
                 <Card.Body>
+                  <div className='d-flex justify-content-between'>
+                  {isHovered?<FontAwesomeIcon icon={faFolderOpen} color='#00ddff' size="lg"/>: <FontAwesomeIcon icon={faFolder} color="#00ddff" size="lg"/>}
+  
+                    {isHovered?<FontAwesomeIcon icon={Star} color='yellow' size="lg"/>:<FontAwesomeIcon icon={faStar} color='yellow' size="lg"/>}
+                  </div>
+                  <Reveal>
                   <Card.Title>{data.title}</Card.Title>
+                  </Reveal>
                   <Card.Subtitle className="mb-2 text-muted">
                     {data.subtitle}
                   </Card.Subtitle>
-                  <Card.Text>
+                  {isHovered ?<> <Card.Text>
                     {data.description}
                   </Card.Text>
+                  <div className='d-flex'>
+                  {data.techStack.map((tech,i)=>(
+                    <div key={i} className='m-1'>
+                      <SkillPill text={tech}/>
+                    </div>
+                  ))}
+                  </div>
                   <div className="social-buttons mt-5">
                     <a href={data.githubLink}>
                       {" "}
@@ -48,7 +76,7 @@ const ProjectCard = ({data}) => {
                         />{" "}
                       </button>
                     </a>:null}
-                  </div>
+                  </div></>:null}
                 </Card.Body>
               </Card>
     
