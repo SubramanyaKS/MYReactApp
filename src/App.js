700419921fdcb403bdc5ac7e2ae564d2b55ea6f7
Subramanya K S <subramanyaks2205@gmail.com>
2024-05-28 06:51:30 +0530

    seperated routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Navbar from "./components/NavBar";
-import About from "./pages/About";
-import Header from "./pages/Header";
-import Education from "./pages/Education";
-import Skills from "./pages/Skills";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Contact from "./pages/Contact";
 import Spinner1 from "./components/Loader";
-import Footer from "./components/Footer";
-import Projects from "./pages/Projects";
-import Experience from "./pages/Experience";
+import Routing from "./routes/Routing";
 function App() {
   const [spinner, setSpinner] = useState(true);
 
@@ -20,24 +11,11 @@ function App() {
   }, []);
 
   return (
-   
     <>
     {spinner ? (
      <Spinner1 />
    ) : (
-     <Router>
-       <Navbar />
-       <Routes>
-         <Route exact path="/" element={<Header />} />
-         <Route exact path="/about" element={<About />} />
-         <Route exact path="/education" element={<Education />} />
-         <Route exact path="/experience" element={<Experience />} />
-         <Route exact path="/project" element={<Projects />} />
-         <Route exact path="/skills" element={<Skills />} />
-         <Route exact path="/contact" element={<Contact />} />
-       </Routes>
-       <Footer />
-     </Router>
+    <Routing/>     
    )}
  </>
   );
