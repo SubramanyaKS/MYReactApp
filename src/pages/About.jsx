d5b40a781fec63a22dcadb5dbb087506ac376013
Subramanya K S <subramanyaks2205@gmail.com>
2024-09-12 07:25:31 +0530

    modified the ui to look better

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,55 +1,41 @@
 import React from "react";
-import { Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { aboutdetails } from "../data/data";
 // import Background from "../components/Background";
 import Heading from "../components/common/Heading";
 import splitWords from "../utils/splitWords";
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
+import { resume, wordVarients } from "../utils/utilFunctions";
+import PillButton from "../components/common/PillButton";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+
 
 const About = () => {
   const description = splitWords(aboutdetails.description);
 
-  const wordVarients = {
-    hidden:{opacity:0},
-    reveal:{opacity:1},
-  }
-
-  const resume = () => {
-    window.open(
-      aboutdetails.resumeLink,
-      "_blank"
-    );
-  };
   return (
     <>
-      <section className="about py-5" id="about">
+      <section className="about py-4" id="about">
         <Heading title="About Me." />
         <Container fluid>
           <Row>
             <Col>
               <img className="MeImg" src={aboutdetails.image} alt="me.." />
-              <br />
             </Col>
             <Col>
-              <motion.p initial="hidden" whileInView="reveal" transition={{staggerChildren:0.02}} className="py-5">
-                {" "}
+              <motion.p initial="hidden" whileInView="reveal" transition={{ staggerChildren: 0.02 }} className="py-5">
                 I am{" "}
                 <span style={{ color: "#00ff00" }} className="font-weight-bold">
                   <b>{aboutdetails.name}, </b>
                 </span>
-                {description.map((ch)=>(
-<motion.span key={ch} transition={{duration:0.5}} variants={wordVarients}>
-  {ch}
-</motion.span>
+                {description.map((ch) => (
+                  <motion.span key={ch} transition={{ duration: 0.5 }} variants={wordVarients}>
+                    {ch}
+                  </motion.span>
 
                 ))}
               </motion.p>
-              <button
-                className="resume rounded-pill"
-                onClick={resume}
-              >
-                Resume
-              </button>
+              <PillButton OnClick={() => resume(aboutdetails.resumeLink)} title="Download Resume" icon={faDownload} />
             </Col>
           </Row>
         </Container>
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {ReactTyped} from "react-typed";
+import { ReactTyped } from "react-typed";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Row,Col,Container} from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 // import Background from '../components/Background';
 import SocialMediaIcons from "../components/SocialMediaIcons";
 import Reveal from "../components/common/Reveal";
-// import Center from "../components/common/Center";
 import { headerdata } from "../data/data";
+import HeaderPhoto from "../components/HeaderPhoto";
 
 const Header = () => {
   return (
@@ -14,41 +14,36 @@ const Header = () => {
       <Container fluid className="py-4">
         <Row className="py-3">
           <Col className="col-lg-6 col-mg-12 py-3 mr-auto">
-          <div className="ps-5">
-            {/* <Center > */}
-            <Reveal>
-            
-            <h1 className="mb-3 " style={{color:"#00ffdd",textAlign:"center"}}>Hi, I'm </h1>
-            
-            </Reveal>
-            {/* b</Center> */}
-            {/* <Center > */}
-            <Reveal>
-            <h1 className="mb-3" style={{color:"#00ff00", fontSize: "50px" }}>{headerdata.name}</h1>
-            </Reveal>
-            {/* </Center> */}
-           <p style={{color:"#00ff00",fontSize:"35px"}}>$
-           <ReactTyped
-              className="typed-text"
-              strings={headerdata.typedarr}
-              typeSpeed={90}
-              backSpeed={90}
-              loop
-              cursorChar= "_"
-            />
-            </p>
-            <SocialMediaIcons/>
-          </div>
+            <div className="ps-5">
+              <Reveal>
+                <h1 className="mb-3 text-center hi">Hi, I'm </h1>
+              </Reveal>
+              <Reveal>
+                <h1 className="mb-3 fs-1">{headerdata.name}</h1>
+              </Reveal>
+              {/* </Center> */}
+              <p className="react-typed">$
+                <ReactTyped
+                  className="typed-text"
+                  strings={headerdata.typedarr}
+                  typeSpeed={90}
+                  backSpeed={90}
+                  loop
+                  cursorChar="_"
+                />
+              </p>
+              <SocialMediaIcons />
+            </div>
           </Col>
           <Col className="col-lg-6 col-mg-12 pb-3 mr-auto">
-          <div className="shadow-lg text-center ">
-          <img className="avatar-img rounded-circle" src={headerdata.image} alt="avatar" height={300}/>
-          </div>
+            <div className="text-center d-flex justify-content-center">
+              <HeaderPhoto image={headerdata.image} />
+            </div>
           </Col>
-          
+
         </Row>
       </Container>
-    {/* <Background id="tsparticles" /> */}
+      {/* <Background id="tsparticles" /> */}
     </section>
   );
 };
