2527871fffb6e090a69a5153c0faeb337956ab23
Subramanya K S <subramanyaks2205@gmail.com>
2024-09-19 07:30:23 +0530

    bug fixed

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -4,8 +4,8 @@ import Card from "react-bootstrap/Card";
 import '../assets/skill.css'
 const SkillsCard = ({ data }) => {
   return (
-    <Card style={{ width: "6rem" }}>
-      <Card.Body>
+    <Card className="skill-card" style={{ width: "6rem" }}>
+      <Card.Body className="skill-card-body">
         <Card.Img src={data.image} />
       </Card.Body>
     </Card>
