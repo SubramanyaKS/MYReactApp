7f123f0928563838586ea5da470226b9cf7d0787
SubramanyaKS <subramanyaks2205@gmail.com>
2025-01-27 15:57:48 +0530

    added styles and data for chatbot

---
+++ src/utils/chatbot.js
@@ -0,0 +1,21 @@
+import chat from '../assets/chatbot.png';
+import user from '../assets/user.png';
+
+export const rules = {
+    skills: ["skills", "expertise", "knowledge", "programming","skill"],
+    projects: ["projects", "portfolio", "work","project"],
+    contact: ["contact", "email", "phone", "reach out"],
+    name:["name","who are you"]
+  };
+
+export const responses = {
+    skills: "He is proficient in web development, React, Node.js, and Python.",
+    projects: "He have worked on some projects which includes phrasepics, weatherapp,react news app .",
+    contact: "You can reach him at connectwithsubbu@gmail.com or on LinkedIn.",
+    name:"I am Subot",
+  };
+
+export const chatbotImages={
+  chatbot:chat,
+  user:user,
+}
